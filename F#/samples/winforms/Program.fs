8a5cab02a2072b5ff0bdda9627a4e795f6a63be3
Gustavo Guerra <gustavo@codebeside.org>
2011-10-23 16:49:17 +0100

    Import FSharpChart 0.55 from http://code.msdn.microsoft.com/FSharpChart-b59073f5

---
+++ F#/samples/winforms/Program.fs
@@ -0,0 +1,13 @@
+﻿namespace MSDN.FSharp.Charting.Tester
+
+open System
+open System.Drawing
+open System.Windows.Forms
+
+module Main =
+
+    [<STAThread>]
+    do
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(false)
+        Application.Run(new CombineLineForm() :> Form) // Can also either CombineLineForm or BoxplotForm
